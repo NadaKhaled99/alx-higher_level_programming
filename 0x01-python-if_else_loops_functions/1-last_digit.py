e491223a47454a8955c9f0fd2060e6dd6943b46c
Nada <nadafarrag619@gmail.com>
2023-11-06 00:53:43 +0200

    file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ modules= number % 10 if number  > 10 else number % -10
 print("Last digit of {} is {} and is ".format(number, modules), end="")
 if modules > 5:
     print("greater than 5")
-elif digit < 5:
-    print("less than 6 and not 0")
-else:
+elif modules == 0:
     print("0")
+else:
+    print("less than 6 and not 0")
