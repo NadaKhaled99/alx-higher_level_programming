fee35431a3eadd060b1fd7770f04fbc457cf94d5
Nada <nadafarrag619@gmail.com>
2023-11-06 00:47:51 +0200

    file

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+import math
+modules= number % 10 if number  > 10 else number % -10
+print("Last digit of {} is {} and is ".format(number, modules), end="")
+if modules > 5:
+    print("greater than 5")
+elif digit < 5:
+    print("less than 6 and not 0")
+else:
+    print("is 0")
