25d9a39072713814a5042b2eb7e195dc545e0e4c
Nada <nadafarrag619@gmail.com>
2023-11-06 21:55:06 +0200

    file

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-	return True
-    else:
-	return False
 def uppercase(str):
-    for c in str:
-	print("{}".format(chr(ord(c)) if not islower(c) else chr(ord(c)) - 32),
-    print("")
+    for g in str:
+	if ord(g) >= ord('a') and ord(g) <= ord('z'):
+            g = chr(ord(g) - 32)
+    print("{}".format(g), end="")
+    print()
