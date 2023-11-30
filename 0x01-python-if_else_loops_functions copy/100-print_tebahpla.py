d29590fac16f41842137b81db6ea3dbf95ba5ace
Nada <nadafarrag619@gmail.com>
2023-11-30 22:45:22 +0200

    file

--- 0x01-python-if_else_loops_functions copy/100-print_tebahpla.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for g in range(25, -1, -1):
-    f = g + ord('A')
-    if g % 2 == 1:
-	f = f + 32
-    print("{}".format(f), end="")
--- 0x01-python-if_else_loops_functions copy/9-print_last_digit.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    num = abs(num) % 10
-    print(num, end-"")
-    return num
