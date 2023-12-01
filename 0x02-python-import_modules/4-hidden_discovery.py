ffb7b386cceb91d9b8d2c0a56ec0faae3370e724
Nada <nadafarrag619@gmail.com>
2023-12-01 02:27:50 +0200

    file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 import hidden_4 as hidden
-if __name__ = "__main__":
-for name in dir(hidden):
-    if name[:2] != "__":
-        print(name)
+if __name__ == "__main__":
+   for name in dir(hidden):
+       if not name[:2] != "__":
+           print(name)
