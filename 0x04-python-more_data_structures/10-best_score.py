0b98c61b0d7376869222f1bcccd7caa83e8ad862
Nada <nadafarrag619@gmail.com>
2023-12-06 23:53:33 +0200

    file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary, keys=a_dictionary.get) if a_dictionary else None
+    if a_dictionary is not None:
+        s= len(a_dictionary.values())
+        if s > 0:
+            se = max(a_dictionary.values())
+            for key in a_dictionary.keys():
+                if a_dictionary[key] == se:
+                    return key
+    return None
