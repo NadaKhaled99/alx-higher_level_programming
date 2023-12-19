1cd54fbe357201fa6227941bee320ca7b8a1058e
Nada <nadafarrag619@gmail.com>
2023-12-20 00:08:43 +0200

    file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size):
+        """new instances of square
+        Args:
+            size: size of the square
+        """
+        self.__size = size
