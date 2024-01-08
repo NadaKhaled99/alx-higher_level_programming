ecac0f45243bfd78321f1f9660360f776951590a
Nada <nadafarrag619@gmail.com>
2024-01-09 00:33:22 +0200

    file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module with class MyList
+"""
+class MyList(list):
+    """Class with print_sorted"""
+    pass
+    def print_sorted(self):
+        """Sorted a list"""
+        print(sorted(list(self)))
