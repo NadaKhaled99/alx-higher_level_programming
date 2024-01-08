5e1e6f00a7e26dd6df0c6a3f2dd22d3fead93656
Nada <nadafarrag619@gmail.com>
2024-01-09 00:56:37 +0200

    file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+================================
+module is_same_class
+================================
+"""
+def is_same_class(obj, a_class):
+    """Method tor return True if an object is same of a class"""
+    return type(obj) is a_class
