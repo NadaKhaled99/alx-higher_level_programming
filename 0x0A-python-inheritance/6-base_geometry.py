267a84dc7e5b37d7540d6975a30adc295161c1e7
Nada <nadafarrag619@gmail.com>
2024-01-09 01:17:31 +0200

    file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+===================================
+class BaseGeometry
+===================================
+"""
+class BaseGeometry:
+    @classmethod
+    def area(self):
+        """for calculated area"""
+        raise Exception("area() is not implemented")
