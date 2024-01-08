aa2a6fff608e06dc030863d0cbb9c62638fb945a
Nada <nadafarrag619@gmail.com>
2024-01-09 00:29:14 +0200

    file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+=============================
+Module with method lookup
+=============================
+"""
 def lookup(obj):
+    """ return the attributes for an object"""
     return dir(obj)
