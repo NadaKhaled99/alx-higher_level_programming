0d3dba1a0b89a86b39529968bf33ece2bd72dccd
Nada <nadafarrag619@gmail.com>
2024-01-09 22:53:36 +0200

    file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defines a JSON file-writing fn"""
+
+
 import json
 
 
+
 def save_to_json_file(my_obj, filename):
-    """Write an object to a file-text using JSON"""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
