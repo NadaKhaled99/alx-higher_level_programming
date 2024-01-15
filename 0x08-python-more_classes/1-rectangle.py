edf5bbfa44b5a5b85bda8b44bf677050dc6676cb
Nada <nadafarrag619@gmail.com>
2024-01-16 01:44:50 +0200

    file

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define Rectangle class"""
+
+
+class Rectangle:
+    """rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """a new Rectangle
+
+        Args:
+            width: The width of the new rectangle
+            height: The height of the new rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
