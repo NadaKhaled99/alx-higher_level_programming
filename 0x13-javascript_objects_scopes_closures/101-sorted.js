c296275f7de0e9ee64f0b15e2630826c1c57ed35
mohamed khaled <moh_32.khaled@yahoo.com>
2024-03-13 04:24:05 +0200

    file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@ const { dict } = require('./101-data');
 const convertedArrr = Object.entries(dict);
 const newObje = {};
 convertedArrr.forEach(element => {
-  newObje[element[1]] ? newObje[element[1]].push(element[0]) : newObj[eelement[1]] = [element[0]];
+  newObje[element[1]] ? newObje[element[1]].push(element[0]) : newObje[element[1]] = [element[0]];
 });
 
 console.log(newObje);
