faa3b029d3f4249bc03e4fcc731472baafa2bda5
Nada <nadafarrag619@gmail.com>
2024-03-14 00:31:18 +0200

    file2

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+const convertedArrr = Object.entries(dict);
+const newObje = {};
+convertedArrr.forEach(element => {
+  newObje[element[1]] ? newObje[element[1]].push(element[0]) : newObje[element[1]] = [element[0]];
+});
+
+console.log(newObje);
