c137947780357c261118bfda4a0d80ee0cddabf6
Nada Khaled <100424417+NadaKhaled99@users.noreply.github.com>
2024-03-13 04:47:39 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const { dict } = require('./101-data');
-const convertedArrr = Object.entries(dict);
-const newObje = {};
-convertedArrr.forEach(element => {
-  newObje[element[1]] ? newObje[element[1]].push(element[0]) : newObje[element[1]] = [element[0]];
-});
-
-console.log(newObje);
