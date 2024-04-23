4f4f7d5c5d8c5808bb052f97cda09219e3ab1085
Nada <nadafarrag619@gmail.com>
2024-04-23 23:15:43 +0200

    file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,6 @@
-§
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if it was received
+});
